af4089af4c6d092901cf52bc33fde6f53ab3eab9
abdelkrim <abdelkarimmassoui@gmail.com>
2026-09-11 21:40:57 +0100

    add tiltle - x label - ylabel

--- pandas-data.py
+++ pandas-data.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 
 x = np.array(['eurusd', 'gbpusd','gold'])
 y = np.array([1453,564,873])
-plt.bar(x, y)
+plt.barh(x, y, color='green', height=0.1)
+plt.title('Currency and Commodity Prices')
+plt.xlabel('Price')
+plt.ylabel('Currency/Commodity')
 plt.show()
 plt.savefig('chart.png')  # Save as image
